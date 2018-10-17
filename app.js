453af20704c37427d8b719382173dbf24eb4e433
mark sauer-utley <mark.utley.713@gmail.com>
2018-10-17 07:53:43 -0400

    require os to get username

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
  console.log('Starting App.');
+
+ const fs = require('fs');
+ const os = require('os');
+
+ let user = os.userInfo();
+
+
+ fs.appendFile('greetings.txt', `Hello ${user.username}!`, (err) => {
+   if (err) {
+     console.log('Unable to write to file.');
+   } else {
+     console.log('Successfully appended file.')
+   }
+ });
